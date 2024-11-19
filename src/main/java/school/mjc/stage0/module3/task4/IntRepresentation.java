9e7f471a4616ca7a5eb5280f2c1aa285f0341724
mrazduev <mrazduev@gmail.com>
2024-11-19 11:23:00 +0300

    Module 3. Task 4. Primitives

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99, h = 104, a = 97, r = 114;
+
+        System.out.println((char)c);
+        System.out.println((char)h);
+        System.out.println((char)a);
+        System.out.println((char)r);
     }
 }
